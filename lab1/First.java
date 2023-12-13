c9e63b8bbfda43d1838622f418ac6bd78983ab46
sandeep kumar <66101879+kaShsanDeep@users.noreply.github.com>
2023-12-13 19:16:41 +0530

    Add files via upload

---
+++ lab1/First.java
@@ -0,0 +1,12 @@
+public class First {
+      
+      public static void main (String args[]){
+
+            System.out.println("Hello world.");
+	
+		}
+      
+}
+// To run this programm on the command prompt we need to compile it first using " First.c  "
+
+
