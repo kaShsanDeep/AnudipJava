50a9559ca51ed234e7bdffe9bf1315fbd2fc9046
sandeep kumar <66101879+kaShsanDeep@users.noreply.github.com>
2024-02-25 18:44:50 +0530

    Add files via upload

---
+++ TaskManagementSystem/TaskManagementSystem/src/main/java/com/Entities/Task.java
@@ -0,0 +1,86 @@
+package com.Entities;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Task {
+
+	@Id
+	private int id;
+	private String title;
+	@Column(length=1500)
+	private String description;
+	private String date;
+	
+	
+	public Task() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Task(int id, String title, String description, String date) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.date = date;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Task [id=" + id + ", title=" + title + ", description=" + description + ", date=" + date
+				+ "]";
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getTitle() {
+		return title;
+	}
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public String getDate() {
+		return date;
+	}
+
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	
+	
+	
+	
+	  
+	 
+}
